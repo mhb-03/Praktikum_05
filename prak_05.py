b96d19fac8d04e18c34bad4d13ab8fbfe0556818
Muhammad Haikal Baihaqi <126536313+mhb-04@users.noreply.github.com>
2023-03-17 09:24:14 +0700

    edited_01

--- prak_05.py
+++ prak_05.py
@@ -1,3 +1,10 @@
-a = 5
-b = 10
-c =a + b
+def bubble_sort(array_data):
+    n = len(array_data)
+    for i in range(n):
+        for j in range (n-i-1):
+            if array_data[j] > array_data[j+1]:
+                print(array_data)
+                array_data[j], array_data[j+1] = array_data[j+1], array_data[j]
+    return array_data
+data = [81,94,39,93,67,28,11]
+print(bubble_sort(data))
